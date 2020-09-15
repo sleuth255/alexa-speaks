68c4a7d6da46ec3caeea3d952f549de361d427d6
sleuth255 <sleuth255@hotmail.com>
2020-09-15 16:38:53 -0500

    refresh cookie interval disabled

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.get('/init', function (req, res) {
         acceptLanguage: 'en-US', // optional, override Accept-Language-Header for cookie determination
         amazonPage: 'amazon.com', // optional, override Amazon-Login-Page for cookie determination and referer for requests
         useWsMqtt: false, // optional, true to use the Websocket/MQTT direct push connection
-        refreshCookieInterval: 24*60*60*1000 // optional, cookie refresh interval, set to 0 to disable refresh
+        refreshCookieInterval: 0 // optional (in ms 7*24*60*60*1000 = 7 days cookie refresh interval, set to 0 to disable refresh
     },
     function (err) {
         if (err) {
@@ -196,7 +196,7 @@ app.get('/getdevices', function (req, res) {
                acceptLanguage: 'en-US', // optional, override Accept-Language-Header for cookie determination
                amazonPage: 'amazon.com', // optional, override Amazon-Login-Page for cookie determination and referer for requests
                useWsMqtt: false, // optional, true to use the Websocket/MQTT direct push connection
-               refreshCookieInterval: 24*60*60*1000 // optional, cookie refresh interval, set to 0 to disable refresh
+               refreshCookieInterval: 0 // optional, cookie refresh interval, set to 0 to disable refresh
            },
            function (err) {
                if (err) {
